1f416ab50ffb3c95de631784e1ee21bf96839557
somesh8787 <111287276+somesh8787@users.noreply.github.com>
2022-08-15 18:06:44 +0530

    Add files via upload

---
+++ AtmInterf.java
@@ -0,0 +1,11 @@
+package AtmSystem;
+
+
+    public interface AtmInterf {
+        public void viewBalance();
+        public void withdrawAmount(double withdrawAmount);
+        public void depositAmount(double depositAmount );
+        public  void viewMiniStatement();
+    }
+
+
